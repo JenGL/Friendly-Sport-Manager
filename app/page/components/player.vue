ec1eaf13c247a68edbe04640bbdba83ec747457b
DESKTOP-KCE3Q6L\Gianluca <gianluca.cecchi@cynny.com>
2018-11-25 21:20:45 +0100

    adjusting player css

--- app/page/components/player.vue
+++ app/page/components/player.vue
@@ -1,41 +1,43 @@
 <template>
-    <div v-if="player" class="content-wrapper" >
-        <div class="md-layout">
-            <md-icon class="md-layout-item md-size-5x">account_circle</md-icon>
-            <div class="margin-left-20 md-layout-item md-layout md-alignment-center-left">
-                <div class="md-layout-item">
-                    <p class="md-title">{{player.Name}}</p>
-                    <p class="md-subheading">Role: {{player.Role}}</p>
-                    <p class="md-subheading">Points: {{player.Points}}</p>
-                    <p class="md-subheading">Goals: {{player.Goals}}</p>
+    <div v-if="player" class="md-layout md-alignment-top-center">
+        <div class="md-layout-item content-wrapper">
+            <div class="md-layout general-info">
+                <md-icon class="md-layout-item md-size-5x">account_circle</md-icon>
+                <div class="margin-left-20 md-layout-item md-layout md-alignment-center-left">
+                    <div class="md-layout-item">
+                        <p class="md-title">{{player.Name}}</p>
+                        <p class="md-subheading">Role: {{player.Role}}</p>
+                        <p class="md-subheading">Points: {{player.Points}}</p>
+                        <p class="md-subheading">Goals: {{player.Goals}}</p>
+                    </div>
                 </div>
             </div>
+            <md-table class="md-medium-show" v-model="player.Matches" md-sort="data" md-sort-order="asc" md-card md-fixed-header>
+                <md-table-toolbar>
+                    <h1 class="md-title">Matches</h1>
+                </md-table-toolbar>
+
+                <md-table-row slot="md-table-row" slot-scope="{ item }">
+                    <md-table-cell md-label="ID" md-numeric="">{{ item.id }}</md-table-cell>
+                    <md-table-cell md-label="Data" md-sort-by="data">{{ toData(item.data) }}</md-table-cell>
+                    <md-table-cell md-label="Outcome" md-numeric>{{ outcome(item.points) }}</md-table-cell>
+                    <md-table-cell md-label="Points" md-numeric>{{ item.points }}</md-table-cell>
+                    <md-table-cell md-label="Goals" md-numeric>{{ item.goals }}</md-table-cell>
+                </md-table-row>
+            </md-table>
+
+            <md-table class="md-small-show" v-model="player.Matches" md-sort="data" md-sort-order="asc" md-fixed-header>
+                <md-table-toolbar>
+                    <h1 class="md-title">Matches</h1>
+                </md-table-toolbar>
+
+                <md-table-row slot="md-table-row" slot-scope="{ item }">
+                    <md-table-cell md-label="Data" md-sort-by="data">{{ toData(item.data) }}</md-table-cell>
+                    <md-table-cell md-label="Outcome" md-numeric>{{ outcome(item.points) }}</md-table-cell>
+                    <md-table-cell md-label="Goals" md-numeric>{{ item.goals }}</md-table-cell>
+                </md-table-row>
+            </md-table>
         </div>
-        <md-table class="md-medium-show" v-model="player.Matches" md-sort="data" md-sort-order="desc" md-card md-fixed-header>
-            <md-table-toolbar>
-                <h1 class="md-title">Matches</h1>
-            </md-table-toolbar>
-
-            <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <md-table-cell md-label="ID" md-numeric="">{{ item.id }}</md-table-cell>
-                <md-table-cell md-label="Data" md-sort-by="data">{{ toData(item.data) }}</md-table-cell>
-                <md-table-cell md-label="Outcome" md-numeric>{{ outcome(item.points) }}</md-table-cell>
-                <md-table-cell md-label="Points" md-numeric>{{ item.points }}</md-table-cell>
-                <md-table-cell md-label="Goals" md-numeric>{{ item.goals }}</md-table-cell>
-            </md-table-row>
-        </md-table>
-
-        <md-table class="md-small-show" v-model="player.Matches" md-sort="data" md-sort-order="desc" md-card md-fixed-header>
-            <md-table-toolbar>
-                <h1 class="md-title">Matches</h1>
-            </md-table-toolbar>
-
-            <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <md-table-cell md-label="Data" md-sort-by="data">{{ toData(item.data) }}</md-table-cell>
-                <md-table-cell md-label="Outcome" md-numeric>{{ outcome(item.points) }}</md-table-cell>
-                <md-table-cell md-label="Goals" md-numeric>{{ item.goals }}</md-table-cell>
-            </md-table-row>
-        </md-table>
     </div>
 </template>
 
@@ -64,14 +66,18 @@
                         return "Loss";
                 }
             },
-            toData(data){
-                return new Date(data*1000).toLocaleDateString()
+            toData(data) {
+                return new Date(data * 1000).toLocaleDateString()
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    .general-info{
+        margin: 30px 0;
+    }
+
     .md-icon.md-layout-item {
         width: 120px;
         height: 120px;
@@ -86,19 +92,19 @@
         margin-left: 20px;
     }
 
-    .md-table{
-        display:none;
+    .md-table {
+        display: none;
     }
 
-    @media only screen and (max-width : 960px) {
-        .md-table.md-small-show{
-            display:none;
+    @media only screen and (max-width: 960px) {
+        .md-table.md-small-show {
+            display: flex;
         }
     }
 
-    @media only screen and (min-width : 960px) {
-        .md-table.md-medium-show{
-            display:none;
+    @media only screen and (min-width: 960px) {
+        .md-table.md-medium-show {
+            display: flex;
         }
     }
 </style>
